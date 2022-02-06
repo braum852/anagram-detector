be6153c7a93a28c41600b188db120d79bc13af69
braum852 <julian.teo59@yahoo.com>
2022-02-06 15:37:38 +1000

    Done

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+##Your class, `Anagram` should take a word on initialization, and instances should
+##respond to a `match` method that takes an array of possible anagrams. It should
+##return all matches in an array. If no matches exist, it should return an empty
+##array.
+
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match(array)
+        array.select{|x| x.split("").sort == @word.split("").sort}
+    end
+
+end
+
+## %wsome_words - a shortcut to instantiate an array with items in it
+## chooses specific values based on the conditions set in curly brackets
+## .split a method that splits an array into subtrings based on condition
+##inside () usually for e.g. - "Hello there".split(",") - separates with ,
